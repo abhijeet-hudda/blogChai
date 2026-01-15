fb78576f456358b26e4db1c471876d5f2d598f43
abhijeet-hudda <abhijeethudda9509@gmail.com>
2026-01-15 22:56:51 +0530

    feat: implement Service class for Appwrite integration with CRUD operations and file handling

---
+++ src/appwrite/config.js
@@ -0,0 +1,105 @@
+import conf from "../config/conf";
+import { Client, Databases, ID,Storage,Query } from "appwrite";
+
+export class Service{
+    client = new Client()
+    databases;
+    bucket;
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjecId);
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+        
+    }
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+            return await this.databases.createDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug,{
+                title,
+                content,
+                featuredImage,
+                status,
+                userId
+            }) 
+        } catch (error) {
+            throw error
+        }
+    }
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try {
+            return await this.databases.createDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug,{
+                title,
+                content,
+                featuredImage,
+                status,
+                userId
+            }) 
+        } catch (error) {
+            throw error
+        }
+    }
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug)
+            return true;
+        } catch (error) {
+            throw error
+        }
+    }
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug)
+        } catch (error) {
+            throw error
+        }
+    }
+    async getPosts(quries = [Query.equal("status","active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                quries,
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+            
+        } catch (error) {
+            throw error
+        }
+    }
+    async uploadFile(fileId){
+        try {
+            return await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            
+        } catch (error) {
+            throw error
+        }
+    }
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+
+}
+
