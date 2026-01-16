3d7e6ff669faf8f31b1497ff9d38968bf3143e72
abhijeet-hudda <abhijeethudda9509@gmail.com>
2026-01-16 13:25:48 +0530

    feat: implement authentication flow with Redux and create Header, Footer, and Logout components

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Container from "./container/Container";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+
+export default {
+    Header,
+    LogoutBtn,
+    Container,
+    Footer,
+    Logo
+}
