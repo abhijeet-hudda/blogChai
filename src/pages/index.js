b286600b51fe7cf069f3f4d87d7db03fb83d0214
abhijeet-hudda <abhijeethudda9509@gmail.com>
2026-01-17 11:57:10 +0530

    feat: set up routing for application with AuthLayout and page components

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Login from "./Login.page";
+import Home from "./Home.page";
+import AddPost from "./AddPost.page";
+import AllPosts from "./AllPosts.page";
+import EditPost from "./EditPost.page";
+import Signup from "./Signup.page";
+import Post from "./Post.page";
+
+export default {
+    Home,
+    Login,
+    Signup,
+    AddPost,
+    AllPosts,
+    EditPost,
+    Post
+}
